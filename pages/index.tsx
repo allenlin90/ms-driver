3afb3d15c6962d04c48a3a5487827054bed3749d
allenlin90 <ht016746@gmail.com>
2023-06-29 01:38:52 +0700

    fix: update root redirect route

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,13 @@ import { NextSeo } from 'next-seo';
 import { getSession } from 'next-auth/react';
 import type { GetServerSideProps, NextPage } from 'next';
 
-import { inAppLinks } from '~/constants/side-nav-links';
-
-const unAuthedPath =
-  (inAppLinks.auth?.href ?? '') + (inAppLinks.login?.href ?? '');
-
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getSession(ctx);
 
   return {
     props: {},
     redirect: {
-      destination: !session?.user ? unAuthedPath : inAppLinks.dashboard?.href,
+      destination: !session?.user ? '/auth/login' : '/dashboard',
       temporary: true,
     },
   };
